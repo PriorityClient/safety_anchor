51b2717c0591515115d72ec40a46748835f0677e
Evan Short <evan.short@gmail.com>
2017-02-28 10:31:03 -0500

    ensuring a protocol is defined in anchor tags

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ function anchor_links(string){
   return string
           .replace(/</g, "&lt;")
           .replace(/>/g, "&gt;")
-          .replace(/([a-zA-Z]+:\/\/[A-Za-z0-9-._~:\/?#\[\]@!$&'()*+,;=\`]+[^((\.|\?)($|\s))])|([A-Za-z0-9-._~:\/?#\[\]@!$&'()*+,;=`]+\.(com|gov|net|org|edu))((:|\?|#)[A-Za-z0-9-._~:\/?#\[\]@!$&'()*+,;=`]+[^((\.|\?)($|\s))])*/g, '<a href="$&">$&</a>')
+          .replace(/(([a-zA-Z]+:\/\/)[A-Za-z0-9-._~:\/?#\[\]@!$&'()*+,;=\`]+[^((\.|\?)($|\s))])|([A-Za-z0-9-._~:\/?#\[\]@!$&'()*+,;=`]+\.(com|gov|net|org|edu))((:|\?|#)[A-Za-z0-9-._~:\/?#\[\]@!$&'()*+,;=`]+[^((\.|\?)($|\s))])*/g, function(m, g1, g2, g3, g4, g5){
+            return '<a href="'+(g2?'':'http://')+m+'">'+m+'</a>'
+          })
 }
 
 module.exports = anchor_links;
