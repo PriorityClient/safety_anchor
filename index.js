10e4b2558e3676e832a70c32654c41b300f615d5
Evan Short <evan.short@gmail.com>
2018-04-25 16:13:16 -0400

    links set to target=blank

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function anchor_links(string){
           .replace(/</g, "&lt;")
           .replace(/>/g, "&gt;")
           .replace(/(([a-zA-Z]+:\/\/)[A-Za-z0-9-._~:\/?#\[\]@!$&'()*+,;=\`]+[^((\.|\?)($|\s))])|([A-Za-z0-9-._~:\/?#\[\]@!$&'()*+,;=`]+\.(com|gov|net|org|edu))((:|\?|#)[A-Za-z0-9-._~:\/?#\[\]@!$&'()*+,;=`]+[^((\.|\?)($|\s))])*/g, function(m, g1, g2, g3, g4, g5){
-            return '<a href="'+(g2?'':'http://')+m+'">'+m+'</a>'
+            return '<a target="_blank" href="'+(g2?'':'http://')+m+'">'+m+'</a>'
           })
 }
 
